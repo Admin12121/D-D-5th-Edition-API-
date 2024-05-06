5e1e8aa641bb7f5eb78420c4815c4169b75695ca
Vicky <vickytaj6459@gmail.com>
2024-05-06 09:36:46 +0545

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/D-D-5th-Edition-API-/"
+  base: "/D-D-5th-Edition-API-/",
+  plugins: [react()]
 })
